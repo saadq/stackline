1eb569fa07993ba4696d6d44e337e6200df0266b
saadq <saad@saadq.com>
2020-09-18 18:45:28 -0700

    Increase sleep timeout

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,4 +1,4 @@
-export function sleep(milliseconds: number = 1) {
+export function sleep(milliseconds: number = 3000) {
   return new Promise((resolve) => {
     setTimeout(resolve, milliseconds)
   })
